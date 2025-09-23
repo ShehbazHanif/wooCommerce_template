f99d8b811a4d85fc863bbb01f3f16502b821d287
ShehbazHanif <shahbazhanee@gmail.com>
2025-09-23 11:58:21 +0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Home from "./pages/Home";
 import CheckOut from "./pages/CheckOut";
 import Login from "./pages/Login";
 import Product from "./pages/Product";
-import NewOrder from "./components/NewOrder";
 
 const App = () => {
   return (
@@ -14,9 +13,6 @@ const App = () => {
         <Route path="/check" element={<CheckOut />} />
         <Route path="/login" element={<Login />} />
         <Route path="/product/:productId" element={<Product />} />
-        <Route path="/email" element={<NewOrder />} />
-        <Route path="/newOrder" element={<NewOrder />} />
-        <Route path="/newOrder" element={<NewOrder />} />
       </Routes>
     </BrowserRouter>
   );
